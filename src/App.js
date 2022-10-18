957f2172b3e33d0031c28390702d6ca88414cb77
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-10-18 16:28:49 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Error404 from './pages/Error404'
 
+import Nav from './components/Nav'
+
 function App() {
   return (
     <Router>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/404' element={<Error404 />} />
-        </Routes>
+        <div className="layout">
+            <Nav />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/home' element={<Home />} />
+                <Route path='/404' element={<Error404 />} />
+            </Routes>
+        </div>
     </Router>
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+
+
+function Nav() {
+  return (
+    <nav>
+        <a target="_blank" href="https://ryanbriggs.dev">Portfolio</a>
+    </nav>
+  )
+}
+
+export default Nav
