36ec217da5b225aa777c45f2b0b778cae541add2
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-10-18 16:09:43 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Error404 from './pages/Error404'
+
 function App() {
   return (
-    <div>
-        App
-    </div>
+    <Router>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/404' element={<Error404 />} />
+        </Routes>
+    </Router>
   )
 }
 
