22fbc334f968c10053337b8d9a249b831176849d
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2022-10-21 09:31:25 +0100

    media queries

--- src/components/Colors.jsx
+++ src/components/Colors.jsx
@@ -2,22 +2,22 @@ function Colors(props) {
   return (
     <div className="color-grid">
         <div className="card">
-            <div className="card-content" style={{backgroundColor: props.color1}} onClick={() => {navigator.clipboard.writeText(props.color1)}}><p>{props.color1}</p></div>
+            <div className="card-content pointer" style={{backgroundColor: props.color1}} onClick={() => {navigator.clipboard.writeText(props.color1)}}><p>{props.color1}</p></div>
         </div>
         <div className="card">
-            <div className="card-content" style={{backgroundColor: props.color2}} onClick={() => {navigator.clipboard.writeText(props.color2)}}><p>{props.color2}</p></div>
+            <div className="card-content pointer" style={{backgroundColor: props.color2}} onClick={() => {navigator.clipboard.writeText(props.color2)}}><p>{props.color2}</p></div>
         </div>
         <div className="card">
-            <div className="card-content" style={{backgroundColor: props.color3}} onClick={() => {navigator.clipboard.writeText(props.color3)}}><p>{props.color3}</p></div>
+            <div className="card-content pointer" style={{backgroundColor: props.color3}} onClick={() => {navigator.clipboard.writeText(props.color3)}}><p>{props.color3}</p></div>
         </div>
         <div className="card">
-            <div className="card-content" style={{backgroundColor: props.color4}} onClick={() => {navigator.clipboard.writeText(props.color4)}}><p>{props.color4}</p></div>
+            <div className="card-content pointer" style={{backgroundColor: props.color4}} onClick={() => {navigator.clipboard.writeText(props.color4)}}><p>{props.color4}</p></div>
         </div>
         <div className="card">
-            <div className="card-content" style={{backgroundColor: props.color5}} onClick={() => {navigator.clipboard.writeText(props.color5)}}><p>{props.color5}</p></div>
+            <div className="card-content pointer" style={{backgroundColor: props.color5}} onClick={() => {navigator.clipboard.writeText(props.color5)}}><p>{props.color5}</p></div>
         </div>
         <div className="card">
-            <div className="card-content" style={{backgroundColor: props.color6}} onClick={() => {navigator.clipboard.writeText(props.color6)}}><p>{props.color6}</p></div>
+            <div className="card-content pointer" style={{backgroundColor: props.color6}} onClick={() => {navigator.clipboard.writeText(props.color6)}}><p>{props.color6}</p></div>
         </div>
     </div>
   )
